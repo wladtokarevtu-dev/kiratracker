54ca87498ea66bcfbab3a4e7e6e6c5fb781a617f
wladtokarevtu-dev <wladtokarevtu@gmail.com>
2025-12-03 15:09:46 +0100

    Fix SecurityConfig syntax error

--- src/main/java/de/wlad/kiratracker/SecurityConfig.java
+++ src/main/java/de/wlad/kiratracker/SecurityConfig.java
@@ -12,22 +12,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration
-@EnableWebSecurity
-public class SecurityConfig package de.wlad.kiratracker;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.SecurityFilterChain;
-
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
@@ -52,7 +36,7 @@ public class SecurityConfig {
     public UserDetailsService userDetailsService() {
         UserDetails admin = User.builder()
                 .username("admin")
-                .password(passwordEncoder().encode("admin123")) // ÄNDERE HIER DAS PASSWORT
+                .password(passwordEncoder().encode("admin123"))
                 .roles("ADMIN")
                 .build();
 

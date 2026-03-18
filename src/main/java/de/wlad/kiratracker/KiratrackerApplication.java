70ffad0b51266f37763379b0c0d15f0c5250109a
Wlad <wladtokarevtu@gmail.com>
2026-03-18 10:29:21 +0100

    config: add ntfy settings and enable scheduling/async

--- src/main/java/de/wlad/kiratracker/KiratrackerApplication.java
+++ src/main/java/de/wlad/kiratracker/KiratrackerApplication.java
@@ -2,12 +2,15 @@ package de.wlad.kiratracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
+@EnableAsync
 public class KiratrackerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(KiratrackerApplication.class, args);
     }
-
 }
